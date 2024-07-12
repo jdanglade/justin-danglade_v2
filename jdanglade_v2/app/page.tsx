b89a4221c17cfb22e5651b38516cab462cd6793f
Justin Danglade <49874942+jdanglade@users.noreply.github.com>
2024-07-12 16:59:27 -0400

    Re-arranging homepage elements

--- jdanglade_v2/app/page.tsx
+++ jdanglade_v2/app/page.tsx
@@ -2,112 +2,117 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="flex relative min-h-screen justify-around border-2 pt-24 border-white">
+      <header className="flex-1 flex-col relative justify-start border-2 border-red-500">
+        <div className="flex sticky left-0 top-12 justify-center">Header</div>
+      </header>
+      <main className="flex-1 flex-col justify-start border-2 border-green-500">
+        <div className="flex relative flex-col justify-start px-14">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+            viverra leo mauris, at rutrum velit porttitor aliquet. Integer eu
+            dui ac nisl feugiat convallis eget a dolor. Integer id sollicitudin
+            urna. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+            In id rutrum lorem. Donec feugiat tortor vel diam rutrum, sed varius
+            turpis consequat. In ultrices sapien sit amet maximus convallis.
+            Mauris in nisl risus. In feugiat, nulla eu tristique hendrerit,
+            risus eros convallis lorem, sed varius nibh erat eu mauris.
+          </p>
+          <p>
+            Suspendisse gravida imperdiet vulputate. Suspendisse potenti. Proin
+            semper odio quis mauris efficitur, ut tristique odio bibendum.
+            Praesent eget urna eu ligula ultrices viverra ac et ipsum. Etiam ut
+            facilisis risus. Nullam aliquam tristique enim, id semper nibh
+            viverra a. Curabitur feugiat, sem ut consequat fringilla, lectus
+            justo vestibulum ante, eu dignissim nisi leo vel arcu. Pellentesque
+            nulla justo, consectetur sit amet sagittis sed, sagittis non felis.
+            Cras vestibulum blandit arcu, sit amet convallis metus dignissim
+            nec. Donec sit amet maximus dui, et ultricies tellus. Etiam quis
+            enim tincidunt, scelerisque odio sed, porta nulla. Pellentesque
+            habitant morbi tristique senectus et netus et malesuada fames ac
+            turpis egestas. Morbi ex ipsum, eleifend nec augue in, fermentum
+            ullamcorper velit. Quisque interdum tincidunt lectus, nec rhoncus
+            odio bibendum vel. Proin dictum sit amet tellus non efficitur.
+            Vivamus diam ipsum, ullamcorper ullamcorper aliquet vel, fringilla
+            ut erat. Aenean malesuada sit amet erat ut blandit. Nunc quis dui
+          </p>
+          <p>
+            nec ex eleifend venenatis vitae nec lectus. Donec quis lorem
+            condimentum, volutpat tellus sed, mattis lectus. Cras vitae dui eget
+            purus aliquam fermentum. Sed enim risus, blandit non lacus vel,
+            aliquam finibus nibh. Sed rhoncus sollicitudin molestie. Donec
+            posuere aliquam neque. Etiam vestibulum orci eleifend enim
+            ullamcorper euismod. Ut venenatis egestas lorem, rutrum pulvinar
+            libero congue eu. Mauris ut mi leo. Mauris pellentesque fringilla
+            dolor, sed cursus ipsum hendrerit vel. Mauris vitae ligula eu urna
+            cursus pretium ac sit amet augue. Quisque vulputate condimentum
+            augue, ut dictum turpis consequat nec. Duis efficitur ligula mi, non
+            faucibus nisl suscipit semper. Duis fermentum maximus orci sed
+            egestas. Suspendisse a est aliquam, egestas lorem eu, consectetur
+            ipsum. Nulla ut finibus mauris. Vivamus vel porta purus. Sed
+          </p>
+          <p>
+            consequat iaculis auctor. Maecenas dapibus nec odio vitae luctus.
+            Cras consectetur nulla nec ligula mollis, nec malesuada tellus
+            scelerisque. Vestibulum blandit lobortis pharetra. Aliquam sagittis
+            pulvinar efficitur. Suspendisse massa metus, viverra et tortor eget,
+            vulputate volutpat nunc. Vivamus in est eget ex porttitor dapibus.
+            Nulla gravida nec ante in ornare. Pellentesque vitae faucibus nisi.
+            Curabitur ut consequat nulla, quis egestas velit. Integer fermentum,
+            odio quis euismod auctor, lectus quam imperdiet orci, et lacinia
+            ipsum lorem at enim. Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Maecenas viverra leo mauris, at rutrum velit
+            porttitor aliquet. Integer eu dui ac nisl feugiat convallis eget a
+          </p>
+          <p>
+            dolor. Integer id sollicitudin urna. Interdum et malesuada fames ac
+            ante ipsum primis in faucibus. In id rutrum lorem. Donec feugiat
+            tortor vel diam rutrum, sed varius turpis consequat. In ultrices
+            sapien sit amet maximus convallis. Mauris in nisl risus. In feugiat,
+            nulla eu tristique hendrerit, risus eros convallis lorem, sed varius
+            nibh erat eu mauris. Suspendisse gravida imperdiet vulputate.
+            Suspendisse potenti. Proin semper odio quis mauris efficitur, ut
+            tristique odio bibendum. Praesent eget urna eu ligula ultrices
+            viverra ac et ipsum. Etiam ut facilisis risus. Nullam aliquam
+            tristique enim, id semper nibh viverra a. Curabitur feugiat, sem ut
+            consequat fringilla, lectus justo vestibulum ante, eu dignissim nisi
+            leo vel arcu. Pellentesque nulla justo, consectetur sit amet
+            sagittis sed, sagittis non felis. Cras vestibulum blandit arcu, sit
+            amet convallis metus dignissim nec. Donec sit amet maximus dui, et
+            ultricies tellus. Etiam quis enim tincidunt, scelerisque odio sed,
+            porta nulla. Pellentesque habitant morbi tristique senectus et netus
+            et malesuada fames ac turpis egestas. Morbi ex ipsum, eleifend nec
+            augue in, fermentum ullamcorper velit. Quisque interdum tincidunt
+            lectus, nec rhoncus odio bibendum vel. Proin dictum sit amet tellus
+            non efficitur. Vivamus diam ipsum, ullamcorper ullamcorper aliquet
+            vel, fringilla ut erat. Aenean malesuada sit amet erat ut blandit.
+            Nunc quis dui nec ex eleifend venenatis vitae nec lectus. Donec quis
+            lorem condimentum, volutpat tellus sed, mattis lectus. Cras vitae
+          </p>
+          <p>
+            dui eget purus aliquam fermentum. Sed enim risus, blandit non lacus
+            vel, aliquam finibus nibh. Sed rhoncus sollicitudin molestie. Donec
+            posuere aliquam neque. Etiam vestibulum orci eleifend enim
+            ullamcorper euismod. Ut venenatis egestas lorem, rutrum pulvinar
+            libero congue eu. Mauris ut mi leo. Mauris pellentesque fringilla
+            dolor, sed cursus ipsum hendrerit vel. Mauris vitae ligula eu urna
+            cursus pretium ac sit amet augue. Quisque vulputate condimentum
+            augue, ut dictum turpis consequat nec. Duis efficitur ligula mi, non
+            faucibus nisl suscipit semper. Duis fermentum maximus orci sed
+            egestas. Suspendisse a est aliquam, egestas lorem eu, consectetur
+            ipsum. Nulla ut finibus mauris. Vivamus vel porta purus. Sed
+            consequat iaculis auctor. Maecenas dapibus nec odio vitae luctus.
+            Cras consectetur nulla nec ligula mollis, nec malesuada tellus
+            scelerisque. Vestibulum blandit lobortis pharetra. Aliquam sagittis
+            pulvinar efficitur. Suspendisse massa metus, viverra et tortor eget,
+            vulputate volutpat nunc. Vivamus in est eget ex porttitor dapibus.
+            Nulla gravida nec ante in ornare. Pellentesque vitae faucibus nisi.
+            Curabitur ut consequat nulla, quis egestas velit. Integer fermentum,
+            odio quis euismod auctor, lectus quam imperdiet orci, et lacinia
+            ipsum lorem at enim.
+          </p>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </main>
+    </div>
   );
 }
