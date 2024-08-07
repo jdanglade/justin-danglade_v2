613de4662129811237ca3fb39b1f8c128c611de2
Justin Danglade <49874942+jdanglade@users.noreply.github.com>
2024-08-07 16:37:38 -0400

    modifying icons and images

--- jdanglade_v2/app/page.tsx
+++ jdanglade_v2/app/page.tsx
@@ -12,6 +12,9 @@ import nextJSLogo from "../public/logos/next.svg";
 import nodeJSLogo from "../public/logos/nodejs.svg";
 import reactLogo from "../public/logos/react.svg";
 import sqlLogo from "../public/logos/sql.svg";
+import profilePic from "../public/profile-pic.jpg";
+import fiuPanther from "../public/florida-panther.jpg";
+import mdcKendall from "../public/mdc-kendall-campus.jpg";
 import Link from "next/link";
 
 const techStackLogos = [
@@ -37,13 +40,13 @@ const HomePage = () => {
         <div className="flex flex-col justify-start relative text-slate-100 border-2 border-green-500">
           <div className="flex text-nowrap text-6xl pb-8 border-2 border-blue-500">
             <span className="font-thin">Justin</span>
-            <span className="font-normal">Danglade</span>
+            <span className="font-mono">Danglade</span>
           </div>
-          <span className="text-2xl font-normal border-2 border-blue-500">
+          <span className="font-mono text-2xl font-thin border-2 border-blue-500">
             Front End Developer
           </span>
         </div>
-        <nav className="flex h-56 justify-start border-2 border-green-500">
+        <nav className="flex h-56 justify-start font-mono border-2 border-green-500">
           <ul className="flex flex-col justify-evenly pt-12 px-12 items-center border-2 border-blue-500">
             <li className="">
               <Link href="#top">
@@ -62,7 +65,11 @@ const HomePage = () => {
             <li className="">Photography </li>
           </ul>
           <div className="flex relative justify-center items-center border-2 border-blue-500">
-            <Image src="" alt="Profile Picture" width={45} height={45}></Image>
+            <Image
+              src={profilePic}
+              alt="Profile Picture"
+              className="w-max h-full object-cover rounded-xl"
+            />
           </div>
         </nav>
         <div className="flex flex-row flex-grow-0 relative justify-center py-4 border-2 border-green-500">
@@ -75,13 +82,13 @@ const HomePage = () => {
         </div>
       </header>
 
-      <main
-        id="top"
-        className="flex-1 flex-col justify-start h-full sticky top-12 mx-12 border-2 border-red-500"
-      >
-        <div className="flex flex-col justify-start border-2 border-green-500">
-          <div className="flex-1 flex-col pb-12 border-2 border-blue-500 ">
-            <span className="">
+      <main className="flex-1 flex-col justify-start h-full sticky font-mono top-12 mx-12 border-2 border-red-500">
+        <div
+          id="top"
+          className="flex flex-1 flex-col justify-start border-2 border-green-500"
+        >
+          <div className="flex flex-col border-2 border-blue-500 ">
+            <span className="text-center">
               From a young age I&apos;ve been so curious about how computers
               work. Why are there so many different kinds of computers? Why are
               some computers so unresonably slow and some so responsive it feels
@@ -107,8 +114,8 @@ const HomePage = () => {
                   <Image
                     src={logo}
                     alt="Logo"
-                    width={150}
-                    height={150}
+                    width={75}
+                    height={75}
                     className=""
                   />
                 </span>
@@ -225,28 +232,62 @@ const HomePage = () => {
         </div>
         <div
           id="education"
-          className="flex flex-col relative pt-12 border-2 border-green-500 "
+          className="flex flex-col justify-start items-center relative text-center border-2 border-green-500 "
         >
+          <div className="py-12">
+            <div className="flex justify-center pb-8 border-2 border-blue-500">
+              <span className="text-nowrap text-center text-4xl">
+                Miami-Dade College
+              </span>
+            </div>
+            <Image
+              src={mdcKendall}
+              alt="Miami-Dade College Kendall Campus"
+              className="w-max h-full object-cover rounded-xl"
+            />
+          </div>
+
           <span className="">
-            I was born and raised in Miami Florida but interestingly enough, I
-            don&apos;t speak Spanish, but I do know a couple of programming
-            languages like C/C++ and JavaScript. I got my associates degree at
-            Miami Dade College and while I was there, I took up a job as a
-            tutor. At first, I provided tutoring for CGS1060, a basic Microsoft
-            Office course that was, for the most part, intuitive for most people
-            taking the class. I kept studying, multiple C++, Java, and Python
-            courses and in time I ended up tutoring those classes as well.
-            Seeing other&apos;s mistakes and misunderstandings was instrumental
-            when taking my studies further...
+            I got my associates degree at Miami Dade College and while I was
+            there, I took up a job as a tutor. At first, I provided tutoring for
+            CGS1060, a basic Microsoft Office course that was for the most part
+            intuitive for most people taking the class. I kept studying,
+            multiple C++, Java, and Python courses and in time I ended up
+            tutoring those classes as well. Seeing other&apos;s mistakes and
+            misunderstandings was pretty useful when taking my studies
+            further...
           </span>
+
+          <div className="py-12">
+            <div className="flex justify-center pb-8 border-2 border-blue-500">
+              <span className="text-balance text-center text-4xl">
+                Florida International University
+              </span>
+            </div>
+            <Image
+              src={fiuPanther}
+              alt="Florida International University"
+              className="w-max h-full object-cover rounded-xl"
+            />
+          </div>
+          {/* <div className="flex justify-center pt-8 border-2 border-blue-500">
+            <span className="text-4xl">Florida Internation University</span>
+          </div>
+          <div className="pt-12">
+            <Image
+              src={fiuPanther}
+              alt="Florida International University"
+              className="w-max h-full object-cover rounded-xl"
+            />
+          </div> */}
           <span className="">
             After MDC I moved on to Florida International University to complete
-            my Bachelor&apos;s degree. Meeting so many career driven young
-            adults was nothing short of inspirational. I saw myself in my
-            classmates, they were just like me, confident that all this effort
-            would pay off. A degree in Computer Science was not easy but after
-            being able to complete the curriculum I can&apos;t help but carry
-            some pride around wherever I go.
+            my Bachelor&apos;s degree. The Florida Panthers, that is a cool
+            mascot, I worked hard to get that degree from The Panthers. Branches
+            of math I had never heard of and this time I was&apos;t working and
+            studying at the same time and place. This time I was on the
+            professor&apos;s schedule. Still I can carry some Panther pride
+            because I got my Bachelor Degree there.
           </span>
         </div>
       </main>
