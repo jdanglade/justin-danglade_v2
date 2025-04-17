ded17cd4b9628a4379caee4e3a7efe108abc9f44
Justin Danglade <49874942+jdanglade@users.noreply.github.com>
2025-04-16 22:59:01 -0400

    easing deployment

--- jdanglade_v2/app/Education/page.tsx
+++ jdanglade_v2/app/Education/page.tsx
@@ -1,96 +1,96 @@
-import Image from "next/image";
-import mdc_kendall from "@/public/mdc-kendall-campus.jpg";
-import florida_panther from "@/public/florida-panther.jpg";
+// import Image from "next/image";
+// import mdc_kendall from "@/public/mdc-kendall-campus.jpg";
+// import florida_panther from "@/public/florida-panther.jpg";
 
-const Education: React.FC = () => {
-  return (
-    <div className="flex flex-col justify-start items-center relative w-full h-full z-0 text-center border-2 border-green-500">
-      <div className="flex justify-start items-center relative w-full font-mono text-4xl pb-4 border-2 border-blue-500">
-        <div className="flex justify-center items-center relative w-1/6 text-sm border-2 border-red-500">
-          Back button
-        </div>
-        <div className="flex justify-end sm:justify-center items-center relative w-5/6 text-4xl border-2 border-red-500">
-          Education
-        </div>
-      </div>
-      <div className="flex justify-center items-center relative w-5/6 font-mono text-2xl py-2 border-2 border-blue-500">
-        Miami-Dade College
-      </div>
-      <Image
-        src={mdc_kendall}
-        alt="Miami-Dade College Kendall"
-        className="flex relative object-cover w-5/6 rounded-xl border-2 border-blue-500"
-      />
-      <p className="flex justify-center items-center relative w-5/6 font-mono text-sm py-4 border-2 border-blue-500">
-        I got my associates degree at Miami Dade College and while I was there,
-        I took up a job as a tutor. At first, I tutored for CGS1060, a basic
-        Microsoft Office course that was for the most part intuitive for most
-        people taking the class. I kept studying, multiple C++, Java, and Python
-        courses and in time I ended up tutoring those classes as well. Seeing
-        other&apos;s mistakes and misunderstandings was pretty useful when
-        taking my studies further...
-      </p>
-      <div className="flex justify-center items-center relative w-5/6 font-mono text-2xl py-2 border-2 border-blue-500">
-        Florida International University
-      </div>
+// const Education: React.FC = () => {
+//   return (
+//     <div className="flex flex-col justify-start items-center relative w-full h-full z-0 text-center border-2 border-green-500">
+//       <div className="flex justify-start items-center relative w-full font-mono text-4xl pb-4 border-2 border-blue-500">
+//         <div className="flex justify-center items-center relative w-1/6 text-sm border-2 border-red-500">
+//           Back button
+//         </div>
+//         <div className="flex justify-end sm:justify-center items-center relative w-5/6 text-4xl border-2 border-red-500">
+//           Education
+//         </div>
+//       </div>
+//       <div className="flex justify-center items-center relative w-5/6 font-mono text-2xl py-2 border-2 border-blue-500">
+//         Miami-Dade College
+//       </div>
+//       <Image
+//         src={mdc_kendall}
+//         alt="Miami-Dade College Kendall"
+//         className="flex relative object-cover w-5/6 rounded-xl border-2 border-blue-500"
+//       />
+//       <p className="flex justify-center items-center relative w-5/6 font-mono text-sm py-4 border-2 border-blue-500">
+//         I got my associates degree at Miami Dade College and while I was there,
+//         I took up a job as a tutor. At first, I tutored for CGS1060, a basic
+//         Microsoft Office course that was for the most part intuitive for most
+//         people taking the class. I kept studying, multiple C++, Java, and Python
+//         courses and in time I ended up tutoring those classes as well. Seeing
+//         other&apos;s mistakes and misunderstandings was pretty useful when
+//         taking my studies further...
+//       </p>
+//       <div className="flex justify-center items-center relative w-5/6 font-mono text-2xl py-2 border-2 border-blue-500">
+//         Florida International University
+//       </div>
 
-      <Image
-        src={florida_panther}
-        alt="Florida International University"
-        className="flex w-5/6 relative object-cover rounded-xl border-2 border-blue-500"
-      />
-      <p className="flex flex-col justify-center items-center relative w-5/6 sm:w-4/6 font-mono text-sm py-4 border-2 border-blue-500">
-        At Florida International University I studied the inside of a processor.
-        LC3 was a processor designed to explain how modern processors work
-        today. At FIU we investigated it&apos;s ALUs, registers, assembly
-        language, the process of compilation, different methods of compilation
-        and how higher level languages utilize them. There were branches of math
-        I had never heard of, computer networking architectures, and the real
-        functions of an Operating System. The Florida Panthers, that is a pretty
-        cool mascot and I worked hard to get that degree from The Panthers.
-      </p>
-      {/* 
-      <p className="flex justify-center items-center relative w-5/6 font-mono text-sm border-2 border-blue-500">
-        At Florida International University I studied the inside of a processor.
-        LC3 was a processor designed to explain how modern processors work
-        today. At FIU we investigated it&apos;s ALUs, registers, assembly
-        language, the process of compilation, different methods of compilation
-        and how higher level languages utilize them. There were branches of math
-        I had never heard of, computer networking architectures, and the real
-        functions of an Operating System. The Florida Panthers, that is a pretty
-        cool mascot and I worked hard to get that degree from The Panthers.
-      </p> */}
-    </div>
+//       <Image
+//         src={florida_panther}
+//         alt="Florida International University"
+//         className="flex w-5/6 relative object-cover rounded-xl border-2 border-blue-500"
+//       />
+//       <p className="flex flex-col justify-center items-center relative w-5/6 sm:w-4/6 font-mono text-sm py-4 border-2 border-blue-500">
+//         At Florida International University I studied the inside of a processor.
+//         LC3 was a processor designed to explain how modern processors work
+//         today. At FIU we investigated it&apos;s ALUs, registers, assembly
+//         language, the process of compilation, different methods of compilation
+//         and how higher level languages utilize them. There were branches of math
+//         I had never heard of, computer networking architectures, and the real
+//         functions of an Operating System. The Florida Panthers, that is a pretty
+//         cool mascot and I worked hard to get that degree from The Panthers.
+//       </p>
+//       {/*
+//       <p className="flex justify-center items-center relative w-5/6 font-mono text-sm border-2 border-blue-500">
+//         At Florida International University I studied the inside of a processor.
+//         LC3 was a processor designed to explain how modern processors work
+//         today. At FIU we investigated it&apos;s ALUs, registers, assembly
+//         language, the process of compilation, different methods of compilation
+//         and how higher level languages utilize them. There were branches of math
+//         I had never heard of, computer networking architectures, and the real
+//         functions of an Operating System. The Florida Panthers, that is a pretty
+//         cool mascot and I worked hard to get that degree from The Panthers.
+//       </p> */}
+//     </div>
 
-    /* 
-      <p className="flex flex-col justify-center items-center relative w-3/4 font-mono text-sm border-2 border-red-500">
-        I got my associates degree at Miami Dade College and while I was there,
-        I took up a job as a tutor. At first, I tutored for CGS1060, a basic
-        Microsoft Office course that was for the most part intuitive for most
-        people taking the class. I kept studying, multiple C++, Java, and Python
-        courses and in time I ended up tutoring those classes as well. Seeing
-        other&apos;s mistakes and misunderstandings was pretty useful when
-        taking my studies further...
-      </p>
-      <div className="flex flex-col justify-center items-center relative w-5/6 font-mono text-3xl border-2 border-red-500">
-        Florida International University
-      </div>
-      <Image
-        src={florida_panther}
-        alt="Florida International University"
-        className="flex w-60 h-60 relative object-cover rounded-xl border-2 border-red-500"
-      />
-      <p className="flex flex-col justify-center items-center relative w-5/6 font-mono text-sm border-2 border-red-500">
-        At Florida International University I studied the inside of a processor.
-        LC3 was a processor designed to explain how modern processors work
-        today. At FIU we investigated it&apos;s ALUs, registers, assembly
-        language, the process of compilation, different methods of compilation
-        and how higher level languages utilize them. There were branches of math
-        I had never heard of, computer networking architectures, and the real
-        functions of an Operating System. The Florida Panthers, that is a pretty
-        cool mascot and I worked hard to get that degree from The Panthers.
-      </p> */
-  );
-};
+//     /*
+//       <p className="flex flex-col justify-center items-center relative w-3/4 font-mono text-sm border-2 border-red-500">
+//         I got my associates degree at Miami Dade College and while I was there,
+//         I took up a job as a tutor. At first, I tutored for CGS1060, a basic
+//         Microsoft Office course that was for the most part intuitive for most
+//         people taking the class. I kept studying, multiple C++, Java, and Python
+//         courses and in time I ended up tutoring those classes as well. Seeing
+//         other&apos;s mistakes and misunderstandings was pretty useful when
+//         taking my studies further...
+//       </p>
+//       <div className="flex flex-col justify-center items-center relative w-5/6 font-mono text-3xl border-2 border-red-500">
+//         Florida International University
+//       </div>
+//       <Image
+//         src={florida_panther}
+//         alt="Florida International University"
+//         className="flex w-60 h-60 relative object-cover rounded-xl border-2 border-red-500"
+//       />
+//       <p className="flex flex-col justify-center items-center relative w-5/6 font-mono text-sm border-2 border-red-500">
+//         At Florida International University I studied the inside of a processor.
+//         LC3 was a processor designed to explain how modern processors work
+//         today. At FIU we investigated it&apos;s ALUs, registers, assembly
+//         language, the process of compilation, different methods of compilation
+//         and how higher level languages utilize them. There were branches of math
+//         I had never heard of, computer networking architectures, and the real
+//         functions of an Operating System. The Florida Panthers, that is a pretty
+//         cool mascot and I worked hard to get that degree from The Panthers.
+//       </p> */
+//   );
+// };
 
-export default Education;
+// export default Education;
